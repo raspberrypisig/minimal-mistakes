892f053b44568ca7691d3cfedd131cdeb8ebb13a
raspberrypisig <raspberrypisig@gmail.com>
2016-12-05 14:59:01 +1100

    Update mohan.js

--- assets/js/mohan.js
+++ assets/js/mohan.js
@@ -1,6 +1,6 @@
 <script type="text/javascript">
 $(document).ready(function() {
   var date = Date.today().second().thursday();
-  $('nextmeetingdate').text = date;
+  $('nextmeetingdate').html = date;
 });
 </script>
