122e57a9d78583cf951ecb51ecd56e8c38cd509f
raspberrypisig <raspberrypisig@gmail.com>
2016-12-25 20:52:32 +1100

    Update mohan.js

--- assets/js/mohan.js
+++ assets/js/mohan.js
@@ -1,20 +1,23 @@
 $(document).ready(function() {
-  var candidate_date = Date.today().second().saturday();
-  var today_date = Date.today();
-  var meeting_remains_this_month = candidate_date.compareTo(today_date);
   
-  if (meeting_remains_this_month == 0) {
+  (function calculateNextMeetingDate() {
+    var candidate_date = Date.today().second().saturday();
+    var today_date = Date.today();
+    var meeting_remains_this_month = candidate_date.compareTo(today_date);
+  
+    if (meeting_remains_this_month == 0) {
       $('#nextmeetingdate').html("MEETING TODAY! 2pm");
-  }
+    }
   
-  if (meeting_remains_this_month > 0) {
-    $('#nextmeetingdate').html(candidate_date.toString("ddd d MMM yyyy"));
-  }
+    if (meeting_remains_this_month > 0) {
+      $('#nextmeetingdate').html(candidate_date.toString("ddd d MMM yyyy"));
+    }
   
-  if (meeting_remains_this_month < 0) {
-    candidate_date = Date.today().next().month().second().saturday();
-    $('#nextmeetingdate').html(candidate_date.toString("ddd d MMM yyyy"));
-  }
+    if (meeting_remains_this_month < 0) {
+      candidate_date = Date.today().next().month().second().saturday();
+      $('#nextmeetingdate').html(candidate_date.toString("ddd d MMM yyyy"));
+    }  
+  })();
   
 
 });
