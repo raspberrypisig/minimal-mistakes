fd4655f1ee315aedd2ab9e5b26809f9b46d9acfd
raspberrypisig <raspberrypisig@gmail.com>
2017-04-04 17:49:58 +1000

    Update search.js

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,18 +1,29 @@
-
+ $(document).ready(function() {
+  
   window.idx = lunr(function () {
     this.field('title');
     this.field('excerpt');
     this.field('content', {boost: 10});
   });
+
+  window.data = $.getJSON('/searchposts.json');    
   
-  $(document).ready(function() {
-    window.data = $.getJSON('/searchposts.json');
+  window.data.then(function(loaded_data){
+    $.each(loaded_data, function(index, value){
+      window.idx.add($.extend({ "id": index }, value));
+    });
+  });      
+  
+  $("#search-box").keypress(function(event) {
+    // event.preventDefault();
+    var query = $("#search-box").val();
+    var results = window.idx.search(query);
     
-    window.data.then(function(loaded_data){
-      $.each(loaded_data, function(index, value){
-        window.idx.add($.extend({ "id": index }, value));
-      });
-      var boo = 2;
-    });  
+    
+  });
+    
+
+    
+
   });  
    
