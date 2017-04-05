ab484ca157cb2b681fc409c6d92f5a23d093ade5
raspberrypisig <raspberrypisig@gmail.com>
2017-04-05 12:10:09 +1000

    Update search.js

--- assets/js/search.js
+++ assets/js/search.js
@@ -14,11 +14,29 @@
     });
   });      
   
-  $("#search-box").keypress(function(event) {
+  $("#search-box").keydown(function(event) {
     // event.preventDefault();
     var query = $("#search-box").val();
     var results = window.idx.search(query);
+    var resultsdiv = $("#search-results")
+    resultsdiv.empty();
+    var id;
+    var fragment;
+    var data;
     
+    for (item in results) {
+      id = item.ref;
+      data = window.data[id];
+      
+      fragment = '<div class="result-item">
+                  <div class="search-result-item-title"><a href="PLACEHOLDER1">PLACEHOLDER2</a></div>
+                  <div class="search-result-item-excerpt">PLACEHOLDER3</div>
+                  </div>';  
+      fragment = fragment.replace('PLACEHOLDER1', data.url);
+      fragment = fragment.replace('PLACEHOLDER2', data.title);
+      fragment = fragment.replace('PLACEHOLDER3', data.excerpt);
+      resultsdiv.append(fragment);
+    }
     
   });
     
